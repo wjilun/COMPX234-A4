1296d4066bf2c6bc59e27267b57ca189c19e8bea
wjilun <1879430899@qq.com>
2025-06-12 19:49:52 +0800

    calculate_md method
    define this method to calculate_md to later check

--- Client/UDPclient.py
+++ Client/UDPclient.py
@@ -2,7 +2,19 @@ import socket
 import base64
 import hashlib
 
-def __init__(self, server_host, server_port):
-    self.server_address = (server_host, server_port)
-    self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    self.client_socket.settimeout(1.0)
+class UDPclient:
+    def __init__(self, server_host, server_port):
+       self.server_address = (server_host, server_port)
+       self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+       self.client_socket.settimeout(1.0)
+
+    def calculate_md5(self, filename):
+        hash_md5 = hashlib.md5()
+        try:
+            with open(filename, 'rb') as f:
+                for content in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(content)
+        except Exception as e:
+            print(f"error when calculate: {e}")
+            return None
+        return hash_md5.hexdigest()     
