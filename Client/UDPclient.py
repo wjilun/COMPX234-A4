df918c6e137d580e2ff7787b1c300afd554befd3
wjilun <1879430899@qq.com>
2025-06-12 19:41:47 +0800

    initial sever
    send the connect address to connect with sever and settimeout(1.0) for later retry

--- Client/UDPclient.py
+++ Client/UDPclient.py
@@ -1,3 +1,8 @@
 import socket
 import base64
 import hashlib
+
+def __init__(self, server_host, server_port):
+    self.server_address = (server_host, server_port)
+    self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.client_socket.settimeout(1.0)
