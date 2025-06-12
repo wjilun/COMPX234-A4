3bc60a03438f083c8a64401e29f50884e9b66491
wjilun <1879430899@qq.com>
2025-06-12 17:18:01 +0800

    send data to client
    use base64 to decode the file content
    and send to client

--- UDPsever.py
+++ UDPsever.py
@@ -5,7 +5,7 @@ import base64
 import os
 import hashlib
 
-def handle_data_request(data_socket,filename,client_adress):
+def handle_file(data_socket,filename,client_adress):
     try:
         while True:
             data,address=data_socket.recvfrom(1024)
@@ -18,6 +18,14 @@ def handle_data_request(data_socket,filename,client_adress):
                     with open(filename, 'rb') as f:
                         f.seek(s)
                         content= f.read(e - s + 1)
+                    encoded_cont = base64.b64encode(content).decode('utf-8')
+                    response = f"FILE {filename} OK START {s} END {e} DATA {encoded_cont}"
+                    data_socket.sendto(response.encode('utf-8'), address)
+                except Exception as e:
+                    print(f"error when get data information: {e}")
+            
+
+
 
 
     
