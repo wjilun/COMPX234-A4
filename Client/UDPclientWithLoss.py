5697cb31cde519d9ec605019ec5961c1a74ec910
wjilun <1879430899@qq.com>
2025-06-12 22:32:46 +0800

    change send_request_with_retry
    simulate the situation of loss packet

--- Client/UDPclientWithLoss.py
+++ Client/UDPclientWithLoss.py
@@ -8,3 +8,27 @@ class UDPclientWithLoss(UDPclient.UDPclient):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_socket.settimeout(1.0)
         self.packet_loss_rate = packet_loss_rate
+
+    def send_request_with_retry(self, message, target_address=None, max_retries=5):
+        if target_address is None:
+            target_address = self.server_address
+        initial_timeout = 1.0
+        for retry in range(max_retries):
+            try:
+                if random.random() >= self.packet_loss_rate:
+                    self.client_socket.sendto(message.encode('utf-8'), target_address)
+                else:
+                    print(f"loss: {message[:10]}...")
+                    continue
+                timeout = initial_timeout * (2 ** retry)
+                self.client_socket.settimeout(timeout)
+                if random.random() < self.packet_loss_rate:
+                    print("loss(accept)")
+                    raise socket.timeout()    
+                response, _ = self.client_socket.recvfrom(4096)
+                return response.decode('utf-8')
+            except socket.timeout:
+                print(f"time out {retry+1}/{max_retries},beyond time: {timeout} 秒")
+            except Exception as e:
+                print(f"error when send: {e}")
+        return None    
